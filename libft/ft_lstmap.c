38db4107dbb8aa137d7c90921305103669bd5c1d
han <bayramincel55@gmail.com>
2026-08-15 12:27:41 +0300

    add mandatory part

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,34 +6,40 @@
 /*   By: haincel <haincel@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/14 15:01:22 by haincel           #+#    #+#             */
-/*   Updated: 2026/08/14 15:01:58 by haincel          ###   ########.fr       */
+/*   Updated: 2026/08/15 12:23:56 by haincel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static void	fail(t_list **lst, void *ncont, void (*del)(void *))
+{
+	if (ncont)
+		del(ncont);
+	ft_lstclear(lst, del);
+}
+
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*head;
 	t_list	*nnode;
-	char	*ncont;
+	void	*ncont;
 
 	if (!f || !lst)
-		return NULL;
+		return (NULL);
 	head = NULL;
 	while (lst)
 	{
 		ncont = f(lst->content);
 		if (!ncont)
 		{
-			ft_lstclear(&head, del);
+			fail(&head, ncont, del);
 			return (NULL);
 		}
 		nnode = ft_lstnew(ncont);
-		if (!nnode || !nnode->content)
+		if (!nnode)
 		{
-			del(nnode);
-			ft_lstclear(&head, del);
+			fail(&head, ncont, del);
 			return (NULL);
 		}
 		ft_lstadd_back(&head, nnode);
